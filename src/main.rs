21241aa1f520e4adac0774923d4d7fce1038cc00
BCzarkowski <bartczarkowski@gmail.com>
2024-11-24 16:51:47 +0100

    przykładowe użycie stosu (do przerobienia na testy w przyszłości)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use update::Updatable;
+use ustack::UStack;
+
+mod update;
+mod ustack;
+
 fn main() {
-    println!("Hello, world!");
+    let mut ustack: UStack<i32> = UStack::new();
+    let push_5 = ustack.push(5);
+    let pop = ustack.pop();
+
+    ustack.apply_update(push_5);
+    println!("Current top: {}", ustack.top());
+    let push_7 = ustack.push(7);
+    println!("Current top: {}", ustack.top());
+    ustack.apply_update(push_7);
+    println!("Current top: {}", ustack.top());
+    ustack.apply_update(pop);
+    println!("Current top: {}", ustack.top());
 }
