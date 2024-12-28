3fa4d39a21b6526205a5827f3ea41ead344ef24c
Froxyy-dev <mateusz.winiarek1@gmail.com>
2024-12-28 11:20:28 +0100

    add unit tests for umessage

---
+++ tests/umessage_tests.rs
@@ -0,0 +1,35 @@
+use shared_state_machine::umessage::UMessage;
+use shared_state_machine::update::Updatable;
+use shared_state_machine::ustack::{UStack, UStackUpdate};
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple_operations() {
+        let mut ustack: UStack<i32> = UStack::new();
+        let push_5 = ustack.push(5);
+
+        let umessage = UMessage::new(0, 0, &push_5).unwrap();
+        let message_update = umessage.get_update::<UStackUpdate<i32>>().unwrap();
+
+        ustack.apply_update(message_update);
+        assert_eq!(*ustack.top(), 5);
+    }
+
+    #[test]
+    fn recursive_operations() {
+        let mut ustack: UStack<UStack<i32>> = UStack::new();
+        ustack.apply_update(ustack.push(UStack::new()));
+
+        let inner_update = ustack.top_mut().push(5);
+        let recursive_update = ustack.create_recursive(inner_update);
+
+        let umessage = UMessage::new(0, 0, &recursive_update).unwrap();
+        let message_update = umessage.get_update::<UStackUpdate<UStack<i32>>>().unwrap();
+
+        ustack.apply_update(message_update);
+        assert_eq!(*ustack.top().top(), 5);
+    }
+}
