f5cbb67ad2cdbe6286f47f261e50ede3f7ce4cfc
Froxyy-dev <mateusz.winiarek1@gmail.com>
2024-12-29 14:49:05 +0100

    server handles groups

--- src/server.rs
+++ src/server.rs
@@ -1,25 +1,54 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::io::{self, Write};
+use std::sync::{Arc, Mutex};
 use tokio::{
     io::{AsyncBufReadExt, AsyncRead, AsyncWrite, AsyncWriteExt, BufReader},
     net::TcpListener,
     sync::broadcast,
 };
 
-async fn run_server() {
-    let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
-    println!("Server is listening on 127.0.0.1:7878.");
+#[derive(Debug)]
+pub struct Group {
+    id: u32,
+    broadcast_tx: broadcast::Sender<String>,
+    current_packet_number: u32,
+    updates_history: Vec<String>,
+}
 
-    let (tx, _rx) = broadcast::channel(10);
+#[derive(Debug)]
+pub struct ServerState {
+    groups: HashMap<u32, Arc<Mutex<Group>>>,
+}
+
+impl ServerState {
+    pub fn new() -> Self {
+        Self {
+            groups: HashMap::new(),
+        }
+    }
+}
+
+pub async fn run_server() {
+    let server_state: Arc<Mutex<ServerState>> = Arc::new(Mutex::new(ServerState::new()));
+    let server_state_clone = server_state.clone();
+
+    tokio::spawn(async move {
+        run_server_with_state(server_state_clone).await;
+    });
+}
+
+pub async fn run_server_with_state(state: Arc<Mutex<ServerState>>) {
+    let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
 
     loop {
-        let (mut socket, addr) = listener.accept().await.unwrap();
-
-        let tx = tx.clone();
-        let rx = tx.subscribe();
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+        let state = state.clone();
 
         tokio::spawn(async move {
             let (reader, writer) = socket.split();
 
-            handle_connection(reader, writer, addr, tx, rx)
+            handle_connection(reader, writer, state)
                 .await
                 .expect("Failed to handle connection.");
         });
@@ -29,9 +58,7 @@ async fn run_server() {
 pub async fn handle_connection<Reader, Writer>(
     reader: Reader,
     mut writer: Writer,
-    addr: std::net::SocketAddr,
-    tx: broadcast::Sender<(String, std::net::SocketAddr)>,
-    mut rx: broadcast::Receiver<(String, std::net::SocketAddr)>,
+    state: Arc<Mutex<ServerState>>,
 ) -> std::io::Result<()>
 where
     Reader: AsyncRead + Unpin,
@@ -40,21 +67,66 @@ where
     let mut reader = BufReader::new(reader);
     let mut line = String::new();
 
+    if reader.read_line(&mut line).await.unwrap() == 0 {
+        return Ok(());
+    }
+
+    let group_id: u32 = line.trim().parse().unwrap();
+    line.clear();
+
+    // Retrieve or create the group
+    let group = {
+        let mut state_lock = state.lock().unwrap();
+        state_lock
+            .groups
+            .entry(group_id)
+            .or_insert_with(|| {
+                let (tx, _rx) = broadcast::channel(16);
+                Arc::new(Mutex::new(Group {
+                    id: group_id,
+                    broadcast_tx: tx,
+                    current_packet_number: 0,
+                    updates_history: vec![],
+                }))
+            })
+            .clone()
+    };
+
+    // Send updates history to the client
+    let history = {
+        let group_lock = group.lock().unwrap();
+        group_lock.updates_history.clone()
+    };
+    for update in history {
+        let update = format!("{}\n", update);
+        writer.write_all(update.as_bytes()).await?;
+    }
+
+    let tx = {
+        let group_lock = group.lock().unwrap();
+        group_lock.broadcast_tx.clone()
+    };
+
+    let mut rx = tx.subscribe();
+
     loop {
         tokio::select! {
             bytes_read = reader.read_line(&mut line) => {
                 if bytes_read.unwrap() == 0 {
                     break Ok(());
                 }
-                println!("Received: {}", line);
-                tx.send((line.clone(), addr)).unwrap();
+                let update = line.trim().to_string();
                 line.clear();
+
+                // Create a new update and broadcast it
+                let mut group_lock = group.lock().unwrap();
+                group_lock.current_packet_number += 1;
+                group_lock.updates_history.push(update.clone());
+                tx.send(format!("{}\n", update)).unwrap();
             }
             message = rx.recv() => {
-                let (message, _sender) = message.unwrap();
-
-                println!("Sending: {}", message);
-                writer.write_all(message.as_bytes()).await.unwrap();
+                let update = message.unwrap();
+                writer.write_all(update.as_bytes()).await?;
             }
         }
     }
--- tests/server_tests.rs
+++ tests/server_tests.rs
@@ -1,28 +1,102 @@
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-use tokio::io::AsyncWriteExt;
-use tokio::sync::broadcast;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use tokio::{
+    io::{AsyncBufReadExt, AsyncRead, AsyncWrite, AsyncWriteExt, BufReader},
+    net::TcpListener,
+    sync::broadcast,
+};
 
-use shared_state_machine::server::handle_connection;
+use shared_state_machine::server::{run_server_with_state, ServerState};
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use tokio::net::TcpStream;
 
     #[tokio::test]
-    async fn echo_server() {
-        let reader = tokio_test::io::Builder::new()
-            .read(b"Hi there\r\n")
-            .read(b"How are you doing?\r\n")
-            .build();
-        let writer = tokio_test::io::Builder::new()
-            .write(b"Hi there\r\n")
-            .write(b"How are you doing?\r\n")
-            .build();
+    async fn test_group_creation_and_update_broadcast() {
+        // This tests covers following server functionalities:
+        // (1) Server creation.
+        // (2) Creating and joining groups.
+        // (3) Broadcasting messages in a group.
+        // (4) Sending history updates.
+        // (5) TODO: Refusing to update when not correct.
 
-        let (tx, _rx) = broadcast::channel(10);
-        let rx = tx.subscribe();
-        let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 7878);
+        //-- (1) --//
+        let server_state: Arc<Mutex<ServerState>> = Arc::new(Mutex::new(ServerState::new()));
+        let server_state_clone = server_state.clone();
 
-        let _result = handle_connection(reader, writer, addr, tx, rx).await;
+        tokio::spawn(async move {
+            run_server_with_state(server_state_clone).await;
+        });
+        tokio::time::sleep(tokio::time::Duration::from_millis(10)).await;
+
+        //-- (2) --//
+        let addr = "127.0.0.1:7878";
+        let client1 = TcpStream::connect(addr).await.unwrap();
+        let client2 = TcpStream::connect(addr).await.unwrap();
+        let mut client1 = BufReader::new(client1);
+        let mut client2 = BufReader::new(client2);
+
+        client1.write_all(b"1\n").await.unwrap();
+        client2.write_all(b"1\n").await.unwrap();
+
+        let update1 = "Update from client 1\n";
+        let update2 = "Update from client 2\n";
+
+        client1.write_all(update1.as_bytes()).await.unwrap();
+
+        //-- (3) --//
+        let mut line = String::new();
+
+        // Client 1 should receive update.
+        client1.read_line(&mut line).await.unwrap();
+        assert_eq!(line, update1);
+        line.clear();
+
+        // Client 2 should receive update.
+        client2.read_line(&mut line).await.unwrap();
+        assert_eq!(line, update1);
+        line.clear();
+
+        // Client 3 connects do different group.
+        let client3 = TcpStream::connect(addr).await.unwrap();
+        let mut client3 = BufReader::new(client3);
+        client3.write_all(b"2\n").await.unwrap();
+
+        // Client 2 sends and update that should be broadcasted to client 1.
+        client2.write_all(update2.as_bytes()).await.unwrap();
+
+        // Client 1 should receive update.
+        client1.read_line(&mut line).await.unwrap();
+        assert_eq!(line, update2);
+        line.clear();
+
+        // Client 2 should receive update.
+        client2.read_line(&mut line).await.unwrap();
+        assert_eq!(line, update2);
+        line.clear();
+
+        //-- (4) --//
+        // Client 4 connects to group 1.
+        let client4 = TcpStream::connect(addr).await.unwrap();
+        let mut client4 = BufReader::new(client4);
+        client4.write_all(b"1\n").await.unwrap();
+
+        // Client 4 should receive history.
+        client4.read_line(&mut line).await.unwrap();
+        assert_eq!(line, update1);
+        line.clear();
+        client4.read_line(&mut line).await.unwrap();
+        assert_eq!(line, update2);
+        line.clear();
+
+        // Client 3 sends a message and only him should receive it.
+        let update3 = "Update from client 3\n";
+        client3.write_all(update3.as_bytes()).await.unwrap();
+        client3.read_line(&mut line).await.unwrap();
+        assert_eq!(line, update3);
+        line.clear();
     }
 }
