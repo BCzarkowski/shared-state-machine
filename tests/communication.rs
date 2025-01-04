d5e6e6336d7a8d0ae398374e904bd1a46f9e7575
BCzarkowski <bartczarkowski@gmail.com>
2025-01-04 19:29:15 +0100

    communication change for async server

--- tests/communication.rs
+++ tests/communication.rs
@@ -1,34 +1,60 @@
+use shared_state_machine::smap::SMap;
 use shared_state_machine::update::Updatable;
 use shared_state_machine::uvec::UVec;
 
 #[cfg(test)]
 mod tests {
+    use std::thread;
+
+    use shared_state_machine::server::Server;
+
     use super::*;
 
-    #[test]
-    fn two_clients() {
-        // let server: Server = Server::new(7878);
+    #[tokio::test]
+    async fn two_clients() {
+        let server_handle = tokio::spawn(async move {
+            println!("Starting server...");
+            let server = Server::new(7878);
+            server.run().await
+        });
 
-        // let mut map1: SMap<String, i32> = SMap::new(7878, 1);
-        // let mut map2: SMap<String, i32> = SMap::new(7878, 1);
-        // let foo = String::from("foo");
-        // let bar = String::from("bar");
-        // let dog = String::from("dog");
-        // map1.insert(foo.clone(), 1);
-        // map1.insert(bar.clone(), 2);
-        // map1.insert(dog.clone(), 3);
+        tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
 
-        // assert_eq!(map2.get(&foo).unwrap(), Some(&1));
-        // assert_eq!(map2.get(&bar).unwrap(), Some(&2));
-        // assert_eq!(map2.get(&dog).unwrap(), Some(&3));
+        let client_handle = tokio::task::spawn_blocking(|| {
+            println!("Starting blocking client...");
+            let mut map1: SMap<String, i32> = SMap::new(7878, 1);
+            let mut map2: SMap<String, i32> = SMap::new(7878, 1);
 
-        // map2.insert(foo.clone(), 9);
-        // map2.insert(bar.clone(), 8);
-        // map2.insert(dog.clone(), 7);
+            let foo = String::from("foo");
+            let bar = String::from("bar");
+            let dog = String::from("dog");
 
-        // assert_eq!(map1.get(&foo).unwrap(), Some(&9));
-        // assert_eq!(map1.get(&bar).unwrap(), Some(&8));
-        // assert_eq!(map1.get(&dog).unwrap(), Some(&7));
+            println!("Inserting values into map1...");
+            map1.insert(foo.clone(), 1);
+            map1.insert(bar.clone(), 2);
+            map1.insert(dog.clone(), 3);
+
+            println!("Verifying map2 reflects map1 changes...");
+            assert_eq!(map2.get(&foo), Some(1));
+            assert_eq!(map2.get(&bar), Some(2));
+            assert_eq!(map2.get(&dog), Some(3));
+
+            map2.insert(foo.clone(), 9);
+            map2.insert(bar.clone(), 8);
+            map2.insert(dog.clone(), 7);
+
+            println!("Verifying map1 reflects map2 changes...");
+            assert_eq!(map1.get(&foo), Some(9));
+            assert_eq!(map1.get(&bar), Some(8));
+            assert_eq!(map1.get(&dog), Some(7));
+        });
+
+        match client_handle.await {
+            Ok(_) => println!("Blocking client test completed successfully."),
+            Err(e) => eprintln!("Blocking client test failed: {:?}", e),
+        }
+
+        server_handle.abort();
     }
 
     #[test]
