d7014aa0a31026acfbb17d22a742e6449fbfa453
BCzarkowski <bartczarkowski@gmail.com>
2025-01-07 19:01:26 +0100

    smap

--- src/smap.rs
+++ src/smap.rs
@@ -3,16 +3,15 @@ use crate::umap::{UMap, UMapUpdate};
 use crate::umessage::UMessage;
 use crate::update;
 use serde::{Deserialize, Serialize};
-use serde_json::{to_vec, Value};
+use serde_json::to_vec;
 use std::hash::Hash;
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::sync::atomic::AtomicU32;
 use std::sync::mpsc::channel;
+use std::sync::mpsc::Sender;
 use std::sync::{mpsc, Arc, Mutex};
-use std::thread;
-use tokio::net::tcp;
-use tokio::sync::mpsc::error;
+use std::{result, thread};
 use tokio_util::bytes::BytesMut;
 use tokio_util::codec::{Decoder, Encoder, LengthDelimitedCodec};
 use update::Updatable;
@@ -23,7 +22,6 @@ pub struct SMap<K: Eq + Hash + Clone + Serialize, T: Updatable + Clone + Seriali
     group_id: u32,
     connection: TcpStream,
     receiver: mpsc::Receiver<ResponseType>,
-    name: String,
 }
 
 enum ResponseType {
@@ -31,38 +29,63 @@ enum ResponseType {
     Rejected,
 }
 
-fn send_client_message<W: Write>(message: ClientMessage, writer: &mut W) -> () {
-    let serialized = to_vec(&message).unwrap();
+pub enum SError {
+    ConnectionError(String),
+    ServerError(String),
+    InternalError(String),
+}
+pub type Result<T> = result::Result<T, SError>;
+
+fn to_connection_error<T: ToString>(error: T) -> SError {
+    SError::ConnectionError(error.to_string())
+}
+
+fn to_server_error<T: ToString>(error: T) -> SError {
+    SError::ServerError(error.to_string())
+}
+
+fn to_internal_error<T: ToString>(error: T) -> SError {
+    SError::InternalError(error.to_string())
+}
+
+fn send_client_message<W: Write>(message: ClientMessage, writer: &mut W) -> Result<()> {
+    let serialized = to_vec(&message).map_err(to_internal_error)?;
     let mut framed = BytesMut::new();
     LengthDelimitedCodec::new()
         .encode(serialized.into(), &mut framed)
         .unwrap();
-    writer.write_all(&framed).unwrap();
-    writer.flush().unwrap();
+    writer.write_all(&framed).map_err(to_internal_error)?;
+    writer.flush().map_err(to_internal_error)?;
+    Ok(())
 }
 
-fn receive_server_message<R: Read>(reader: &mut R) -> Result<ServerMessage, std::io::Error> {
+fn stream_server_messages<R: Read>(reader: R, sender: Sender<ServerMessage>) -> Result<()> {
     let mut codec = LengthDelimitedCodec::new();
     let mut buffer = BytesMut::new();
+    let mut reader = reader;
 
-    // Read data from the socket until we get a full frame
     let mut temp_buffer = [0; 1024];
     loop {
-        let bytes_read = reader.read(&mut temp_buffer)?;
-        if bytes_read == 0 {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::UnexpectedEof,
-                "Connection closed",
-            ));
-        }
+        let status = (|| -> Result<()> {
+            let bytes_read = reader.read(&mut temp_buffer).map_err(to_internal_error)?;
+            if bytes_read == 0 {
+                return Err(SError::ServerError("Connection closed".to_owned()));
+            }
 
-        buffer.extend_from_slice(&temp_buffer[..bytes_read]);
+            buffer.extend_from_slice(&temp_buffer[..bytes_read]);
 
-        // Try to decode a frame
-        if let Some(frame) = codec.decode(&mut buffer).unwrap() {
-            // Deserialize the frame into the ServerMessage type
-            let message: ServerMessage = serde_json::from_slice(&frame)?;
-            return Ok(message);
+            let frame = codec.decode(&mut buffer).map_err(to_internal_error)?;
+
+            if let Some(frame) = frame {
+                let message: ServerMessage =
+                    serde_json::from_slice(&frame).map_err(to_internal_error)?;
+                sender.send(message).map_err(to_internal_error)?;
+            }
+            Ok(())
+        })();
+        if let Err(error) = status {
+            drop(sender);
+            return Err(error);
         }
     }
 }
@@ -73,97 +96,93 @@ where
     T: Updatable + Clone + Serialize + for<'de> Deserialize<'de> + Send + 'static,
     <T as Updatable>::Update: Serialize + for<'de> Deserialize<'de> + Send + 'static,
 {
-    pub fn new(port: u16, group: u32, name: String) -> Self {
+    pub fn new(port: u16, group: u32) -> Result<Self> {
         let map = Arc::new(Mutex::new(UMap::new()));
-        let map_clone = map.clone();
-        let connection = TcpStream::connect(format!("127.0.0.1:{}", port)).unwrap();
-        let tcp_stream = connection.try_clone().unwrap();
+        let tcp_stream =
+            TcpStream::connect(format!("127.0.0.1:{}", port)).map_err(to_connection_error)?;
         let last_packet_number = Arc::new(AtomicU32::new(0));
-        let packet_id = last_packet_number.clone();
-        let (sender, receiver) = channel();
-        let nname = name.clone();
-        // let send_client_message = |message: ClientMessage| {
-        //     let mut tcp_stream = &connection;
-        //     serde_json::to_writer(tcp_stream, &message).unwrap();
-        //     tcp_stream.flush().unwrap();
-        // };
-        {
+        let (server_message_sender, server_message_receiver) = channel();
+        let _ = {
+            let tcp_stream = tcp_stream.try_clone().map_err(to_internal_error)?;
+            thread::spawn(|| stream_server_messages(tcp_stream, server_message_sender));
+        };
+        let _ = {
             let mut tcp_stream = &tcp_stream;
-            send_client_message(ClientMessage::JoinGroup(group), &mut tcp_stream);
-        }
-        {
-            let mut tcp_stream = &tcp_stream;
-            match receive_server_message(&mut tcp_stream).unwrap() {
-                ServerMessage::Correct => (),
-                _ => panic!("Expected ServerMessage::Correct"),
+            send_client_message(ClientMessage::JoinGroup(group), &mut tcp_stream)
+        }?;
+        let _ = {
+            let message = server_message_receiver.recv().map_err(to_internal_error)?;
+            match message {
+                ServerMessage::Correct => Ok(()),
+                _ => Err(SError::ConnectionError(
+                    "Server didn't accept join request".to_owned(),
+                )),
             }
-        }
-        dbg!("Server accepted connection | ".to_owned() + &name);
+        }?;
+        let (response_sender, response_receiver) = channel();
+        let result = {
+            let connection = tcp_stream.try_clone().map_err(to_internal_error)?;
+            SMap {
+                map: map.clone(),
+                last_packet_number: last_packet_number.clone(),
+                connection,
+                group_id: group,
+                receiver: response_receiver,
+            }
+        };
         thread::spawn(move || {
             let mut can_send_rejected = true;
             let mut should_send_reject_after_update = false;
             loop {
-                dbg!("Enter another loop of receiving | ".to_owned() + &name);
-                let mut tcp_stream = &tcp_stream;
-                let message = receive_server_message(&mut tcp_stream).unwrap();
-                match message {
-                    ServerMessage::Update(umessage) => {
-                        dbg!(
-                            "Received update from server | ".to_owned()
-                                + &name
-                                + " | "
-                                + &group.to_string()
-                                + " | "
-                                + &umessage.packet_id.to_string()
-                        );
-                        packet_id
-                            .store(umessage.packet_id + 1, std::sync::atomic::Ordering::Relaxed);
-                        dbg!(packet_id.load(std::sync::atomic::Ordering::Relaxed));
-                        let update = umessage.get_update().unwrap();
-                        map.lock().unwrap().apply_update(update);
-                        if should_send_reject_after_update {
-                            sender.send(ResponseType::Rejected).unwrap();
-                        } else {
-                            can_send_rejected = true;
+                let status = (|| -> Result<()> {
+                    let message = server_message_receiver.recv().map_err(to_internal_error)?;
+                    match message {
+                        ServerMessage::Update(umessage) => {
+                            last_packet_number.store(
+                                umessage.packet_id + 1,
+                                std::sync::atomic::Ordering::Relaxed,
+                            );
+                            let update = umessage.get_update().map_err(to_internal_error)?;
+                            map.lock().unwrap().apply_update(update);
+                            if should_send_reject_after_update {
+                                response_sender
+                                    .send(ResponseType::Rejected)
+                                    .map_err(to_internal_error)
+                            } else {
+                                can_send_rejected = true;
+                                Ok(())
+                            }
+                        }
+                        ServerMessage::Correct => response_sender
+                            .send(ResponseType::Accepted)
+                            .map_err(to_internal_error),
+                        ServerMessage::Error => {
+                            if can_send_rejected {
+                                can_send_rejected = false;
+                                response_sender
+                                    .send(ResponseType::Rejected)
+                                    .map_err(to_internal_error)
+                            } else {
+                                should_send_reject_after_update = true;
+                                Ok(())
+                            }
                         }
                     }
-                    ServerMessage::Correct => {
-                        sender.send(ResponseType::Accepted).unwrap();
-                    }
-                    ServerMessage::Error => {
-                        dbg!(
-                            "Received Rejected | ".to_owned()
-                                + &name
-                                + " | "
-                                + &can_send_rejected.to_string()
-                        );
-                        if can_send_rejected {
-                            can_send_rejected = false;
-                            sender.send(ResponseType::Rejected).unwrap();
-                        } else {
-                            should_send_reject_after_update = true;
-                        }
-                    }
-                };
+                })();
+                if let Err(_) = status {
+                    let _ = tcp_stream.shutdown(std::net::Shutdown::Both);
+                    break;
+                }
             }
         });
-        SMap {
-            map: map_clone,
-            last_packet_number,
-            connection,
-            group_id: group,
-            receiver,
-            name: nname,
-        }
+        Ok(result)
     }
 
     fn publish_update(&mut self, update: UMapUpdate<K, T>) -> () {
-        dbg!("Publishing an update | ".to_owned() + &self.name);
         loop {
             let packet_id = self
                 .last_packet_number
                 .load(std::sync::atomic::Ordering::Relaxed);
-            dbg!("Sending update | ".to_owned() + &self.name + " | " + &packet_id.to_string());
             let group_id = self.group_id;
             let umessage = UMessage::new(group_id, packet_id, &update).unwrap();
             let message = ClientMessage::Update(umessage);
@@ -172,7 +191,6 @@ where
             match self.receiver.recv() {
                 Ok(response) => {
                     if let ResponseType::Accepted = response {
-                        dbg!("Published update successfully! | ".to_owned() + &self.name);
                         break;
                     }
                 }
@@ -184,7 +202,6 @@ where
     }
 
     pub fn insert(&mut self, key: K, value: T) -> () {
-        dbg!("Inserting value to smap");
         Self::publish_update(self, UMapUpdate::Insert(key, value))
     }
 
@@ -197,4 +214,12 @@ where
     }
 }
 
-// TODO - structure wrapper for S Structures
+impl<K, T> Drop for SMap<K, T>
+where
+    K: Eq + Hash + Clone + Serialize,
+    T: Updatable + Clone + Serialize,
+{
+    fn drop(&mut self) {
+        let _ = self.connection.shutdown(std::net::Shutdown::Both);
+    }
+}
